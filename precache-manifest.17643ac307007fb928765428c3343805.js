011a9f966074ffc3b20d880ad45e2534b2a7492f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-20 01:33:13 +0000

    Deploy brick-a-pic/brick-a-pic to github.com/brick-a-pic/brick-a-pic.git:gh-pages

---
+++ precache-manifest.17643ac307007fb928765428c3343805.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea508d1c7bce49bf3c8",
+    "url": "/css/app.09edef48.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "12956db54f691321e4ad6615f5caf279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eea508d1c7bce49bf3c8",
+    "url": "/js/app.e0cd1c3c.js"
+  },
+  {
+    "revision": "083f825bd7b63ea74c70",
+    "url": "/js/chunk-vendors.5006c987.js"
+  },
+  {
+    "revision": "9682c10d51ee0f616ec0d889667d9c9e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);

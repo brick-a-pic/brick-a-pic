d2c8fb1819a1afc96cb2b5b7ae733c8da6473f91
Stephen Hannon <stephenhannon4@gmail.com>
2020-02-11 18:15:39 -0500

    Adds settings component

---
+++ src/components/Settings.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-navigation-drawer
+    :value="showDrawer"
+    app
+    clipped
+  >
+    <v-btn color="primary">Open image</v-btn>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'Settings',
+  props: {
+    showDrawer: Boolean,
+  },
+};
+</script>
+
+<style>
+
+</style>

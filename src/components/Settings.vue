9b9a6f87336e04cf586efc51f369ca640e66aa41
Stephen Hannon <stephenhannon4@gmail.com>
2020-02-16 21:41:58 -0500

    Adds upload component

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -2,14 +2,26 @@
   <v-card max-width="20em">
     <v-card-title>Options</v-card-title>
     <v-card-text>
-      <v-btn color="primary">Open image</v-btn>
+      <OpenImage @change="onImageOpen"/>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+import OpenImage from './OpenImage.vue';
+
 export default {
   name: 'Settings',
+  components: {
+    OpenImage,
+  },
+
+  methods: {
+    onImageOpen(image) {
+      console.log(image);
+      // TODO
+    },
+  },
 };
 </script>
 

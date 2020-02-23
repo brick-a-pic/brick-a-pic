f34cb22906060e6cc3a954ddf76f6ea8cdf2c19d
centipeda <joshua.c.guam@gmail.com>
2020-02-23 16:49:03 -0500

    fix linting errors

--- tests/unit/settings.spec.js
+++ tests/unit/settings.spec.js
@@ -1,31 +1,28 @@
 import Vue from 'vue';
-import { mount,
-         createLocalVue
-       } from '@vue/test-utils';
+import {
+  mount,
+  createLocalVue,
+} from '@vue/test-utils';
+import Vuetify from 'vuetify';
 import Settings from '@/components/Settings.vue';
 import OpenImage from '@/components/OpenImage.vue';
-import Vuetify from 'vuetify';
 
 const localVue = createLocalVue();
 Vue.use(Vuetify);
 
 describe('Settings.vue', () => {
-    let vuetify;
-    beforeEach(() => {
-        vuetify = new Vuetify();
-    });
+  let vuetify;
+  beforeEach(() => { vuetify = new Vuetify(); });
 
-    const mountFunc = options => {
-        return mount(Settings, {
-            localVue,
-            vuetify,
-        })
-    };
+  const mountFunc = () => mount(Settings, {
+    localVue,
+    vuetify,
+  });
 
-    const wrapper = mountFunc({
-    });
+  const wrapper = mountFunc({
+  });
 
-    it('contains an OpenImage child component', () => {
-        expect(wrapper.find(OpenImage).exists()).toBe(true);
-    });
+  it('contains an OpenImage child component', () => {
+    expect(wrapper.find(OpenImage).exists()).toBe(true);
+  });
 });

2ead8f17db781c6e9a279234188684ea9be06369
centipeda <joshua.c.guam@gmail.com>
2020-02-23 12:03:21 -0500

    add documentation, remove extraneous elements

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -11,7 +11,12 @@
 <script>
 import ImageProcessor from './ImageProcessor.vue';
 
-function drawToSvg(imageData) {
+function drawToSvg(imageData, svg, x, y) {
+  /* Takes an ImageData object and an SVG element, and draws the
+     ImageData to the SVG element as a bunch of rects. */
+
+  // First we split up ImageData.data (a flat uint_8 array) to separate arrays
+  // of RGB color values
   const red = [];
   const green = [];
   const blue = [];
@@ -20,26 +25,33 @@ function drawToSvg(imageData) {
     green.push(imageData.data[pixel + 1]);
     blue.push(imageData.data[pixel + 2]);
   }
+
+  // how big each 'pixel' will be, in SVG-units
   const pixelHeight = 1;
   const pixelWidth = 1;
-  const baseX = 60;
-  const baseY = 30;
-  const svgUri = 'http://www.w3.org/2000/svg';
+  const svgUri = 'http://www.w3.org/2000/svg'; // needed to create SVG elements
   let tempSquare;
   let colorAttribute;
   let pixelPosition;
-  const svgElement = document.getElementsByClassName('preview-svg')[0];
+
+  // then iterate through every pixelwise position in the image data we have
   for (let yPosition = 0; yPosition < imageData.height; yPosition += 1) {
     for (let xPosition = 0; xPosition < imageData.width; xPosition += 1) {
+      // get the position of the pixel in the flattened array
       pixelPosition = (yPosition * imageData.width) + xPosition;
+
+      // each square in the transformed image will be an independent SVG rect, so create the element
       tempSquare = document.createElementNS(svgUri, 'rect');
+      // use the pixel data to figure out what color it should be
       colorAttribute = `fill: rgb(${red[pixelPosition]},${green[pixelPosition]},${blue[pixelPosition]});`;
+      // then set attributes like size/position/color
       tempSquare.setAttributeNS(null, 'style', colorAttribute);
       tempSquare.setAttributeNS(null, 'height', pixelHeight);
       tempSquare.setAttributeNS(null, 'width', pixelWidth);
-      tempSquare.setAttributeNS(null, 'x', String(baseX + xPosition * pixelWidth));
-      tempSquare.setAttributeNS(null, 'y', String(baseY + yPosition * pixelHeight));
-      svgElement.appendChild(tempSquare);
+      tempSquare.setAttributeNS(null, 'x', String(x + xPosition * pixelWidth));
+      tempSquare.setAttributeNS(null, 'y', String(y + yPosition * pixelHeight));
+      // finally, attach it to the svg element in the DOM
+      svg.appendChild(tempSquare);
     }
   }
 }
@@ -52,7 +64,8 @@ export default {
   },
   methods: {
     onImageSampled(imageData) {
-      drawToSvg(imageData);
+      const svgElement = document.getElementsByClassName('preview-svg')[0];
+      drawToSvg(imageData, svgElement, 80, 40);
     },
   },
 };

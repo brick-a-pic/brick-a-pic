28ad0f80519ddbaa53beeaa07ee9b8b7fa26f178
centipeda <joshua.c.guam@gmail.com>
2020-02-23 16:36:28 -0500

    properly import Vuetify into the unit tests

--- tests/unit/settings.spec.js
+++ tests/unit/settings.spec.js
@@ -1,9 +1,29 @@
-import { shallowMount } from '@vue/test-utils';
+import Vue from 'vue';
+import { mount,
+         createLocalVue
+       } from '@vue/test-utils';
 import Settings from '@/components/Settings.vue';
 import OpenImage from '@/components/OpenImage.vue';
+import Vuetify from 'vuetify';
+
+const localVue = createLocalVue();
+Vue.use(Vuetify);
 
 describe('Settings.vue', () => {
-    const wrapper = shallowMount(Settings);
+    let vuetify;
+    beforeEach(() => {
+        vuetify = new Vuetify();
+    });
+
+    const mountFunc = options => {
+        return mount(Settings, {
+            localVue,
+            vuetify,
+        })
+    };
+
+    const wrapper = mountFunc({
+    });
 
     it('contains an OpenImage child component', () => {
         expect(wrapper.find(OpenImage).exists()).toBe(true);

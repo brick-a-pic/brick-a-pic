0225bcc2ec21d7514cce267c0775081ad727aacb
Jamie Zhang <jzhang17@nd.edu>
2020-02-29 19:28:20 -0500

    New Settings

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -35,17 +35,20 @@
             ></v-text-field>
           </v-flex>
         </v-layout>
+      <ColorPicker @change="OnColorPick"/>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
 import OpenImage from './OpenImage.vue';
+import ColorPicker from './ColorPicker.vue';
 
 export default {
   name: 'Settings',
   components: {
     OpenImage,
+    ColorPicker,
   },
   data: () => ({
     ImageCheck: 0,

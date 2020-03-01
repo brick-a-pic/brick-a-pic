71e4edd537cc42d548c6233ba67fc2d934a41078
Jamie Zhang <jzhang17@nd.edu>
2020-02-29 19:28:20 -0500

    New Settings

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -3,17 +3,20 @@
     <v-card-title>Options</v-card-title>
     <v-card-text>
       <OpenImage @change="onImageOpen"/>
+      <ColorPicker @change="OnColorPick"/>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
 import OpenImage from './OpenImage.vue';
+import ColorPicker from './ColorPicker.vue';
 
 export default {
   name: 'Settings',
   components: {
     OpenImage,
+    ColorPicker,
   },
 
   methods: {

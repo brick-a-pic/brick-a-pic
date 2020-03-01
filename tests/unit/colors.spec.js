c8f8dc4f66ccad2fb534aedb32b7b2f0a61b2271
Stephen Hannon <stephenhannon4@gmail.com>
2020-03-01 16:27:46 -0500

    Fixes test, removes unnecessary changes

--- tests/unit/colors.spec.js
+++ tests/unit/colors.spec.js
@@ -3,6 +3,6 @@ import * as colors from '@/colors';
 describe('colors.js', () => {
   it('matches closest colors', () => {
     expect(colors.closestMatch([255, 255, 255])).toEqual([242, 243, 242]);
-    expect(colors.closestMatch([255, 0, 0])).toEqual([227, 0, 11]);
+    expect(colors.closestMatch([255, 0, 0])).toEqual([196, 40, 27]);
   });
 });

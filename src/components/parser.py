4a2ff6a82c0077edc49d202fe0bd4d11cd606981
Jamie Zhang <jzhang17@nd.edu>
2020-03-01 15:18:46 -0500

    Delete parser.py

--- src/components/parser.py
+++
@@ -1,8 +0,0 @@
-import os
-
-filename = "colors.csv"
-f = open(filename, 'r')
-
-for line in f.readlines():
-    line = line.strip().split()
-    print('[{}, {}, {}],'.format(line[-3][:-1], line[-2][:-1], line[-1]))

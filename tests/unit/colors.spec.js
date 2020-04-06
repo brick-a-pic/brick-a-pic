b6ff3e48d6e2e6210b64de967abcba6ddf29961e
Jamie Zhang <jzhang17@nd.edu>
2020-04-06 14:57:59 -0400

    fixed unit tests

--- tests/unit/colors.spec.js
+++ tests/unit/colors.spec.js
@@ -2,7 +2,7 @@ import * as colors from '@/colors';
 
 describe('colors.js', () => {
   it('matches closest colors', () => {
-    expect(colors.closestMatch([255, 255, 255])).toEqual([242, 243, 242]);
-    expect(colors.closestMatch([255, 0, 0])).toEqual([196, 40, 27]);
+    expect(colors.closestMatch([255, 255, 255], [0, 1, 2])).toEqual([242, 243, 242]);
+    expect(colors.closestMatch([0, 0, 0], [0, 1, 2])).toEqual([99, 95, 97]);
   });
 });
